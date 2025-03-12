927347c2b652ded63b8647cdb137480dd285f399
mjakovljev <milovanjakovljevic78@gmail.com>
2025-03-13 00:58:01 +0100

    Testing deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1>Restaurant</h1>
+      <p>COO,CFO,CIO,CMO,CCO and KAM : Ivan Manojlovic</p>
+      <p>CTO : Milovan Jakovljevic</p>
+    </div>
+  );
 }
 
 export default App;
